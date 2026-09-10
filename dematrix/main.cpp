762fbc932698de67e2573246b1124db7aee48744
Ivan Almada <ivan.almada@me.com>
2026-09-10 10:09:32 -0600

    Added Headers

--- dematrix/main.cpp
+++ dematrix/main.cpp
@@ -1,6 +1,21 @@
+#include <cctype>
+#include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+#if defined(_WIN32)
+    #include <direct.h>
+#else
+    #include <unistd.h>
+    #include <climits>
+#endif
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
     return EXIT_SUCCESS;
 }
