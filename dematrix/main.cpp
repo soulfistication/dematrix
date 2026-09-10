36965fd02ae47ecfec1ae6161abc914437a0b2c6
Ivan Almada <ivan.almada@me.com>
2026-09-10 10:18:09 -0600

    Finished getCurrentDirectory()

--- dematrix/main.cpp
+++ dematrix/main.cpp
@@ -15,6 +15,30 @@
 
 using namespace std;
 
+const char* const kProgramVersion = "0.1.0";
+
+#if defined(_WIN32)
+const char kPathSeparator = '\\';
+#else
+const char kPathSeparator = '/';
+#endif
+
+string getCurrentDirectory() {
+#if defined(_WIN32)
+    cont int kPathBuffSize = 4096;
+    char buff[kPathBuffSize];
+    if (_getcwd(buff, sizeof(buff))) {
+        return string(buff);
+    }
+#else
+    char buff[PATH_MAX];
+    if (getcwd(buff, sizeof(buff))) {
+        return string(buff);
+    }
+#endif
+    return ".";
+}
+
 int main(int argc, const char * argv[]) {
     cout << "Hello, World!\n";
     return EXIT_SUCCESS;
